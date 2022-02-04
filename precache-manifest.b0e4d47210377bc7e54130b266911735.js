c24f9f10d17ebc1c707f249d966e4882f2af3aaa
hulio-pc <ivetal820@gmail.com>
2022-02-04 15:18:11 +0200

    1

---
+++ precache-manifest.b0e4d47210377bc7e54130b266911735.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a24cec5ffe4a15fc97c5182b8f6a286e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b530f008467f8fc4021",
+    "url": "/static/css/main.c1c7d289.chunk.css"
+  },
+  {
+    "revision": "bac6c2c7e81db87e4123",
+    "url": "/static/js/2.99a1fa7c.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.99a1fa7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b530f008467f8fc4021",
+    "url": "/static/js/main.73548951.chunk.js"
+  },
+  {
+    "revision": "032fc75a279aa53db0b9",
+    "url": "/static/js/runtime-main.adeadb3d.js"
+  }
+]);
